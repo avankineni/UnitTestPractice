1b1dabaf63882948c26c5ebf0c76fa7151424f72
avankineni <avankineni@mail.csuchico.edu>
2024-04-24 11:37:38 -0700

    Added unit testing using gcov

--- Password.cpp
+++ Password.cpp
@@ -1,20 +1,50 @@
 #include "Password.h"
 #include <string>
+#include <algorithm>
+#include <cctype>
 
 using std::string;
 
-/*
-  The function receives a string counts how many times the same character 
-  occurs at the beginning of the string, before any other characters (or the
-  end of the string). The function is case-sensitive so 'Z' is different than
-  'z' and any ASCII characters are allowed.
-*/
-int Password::count_leading_characters(string phrase){
-  int repetition = 1;
-  int index = 0;
-  while( index < phrase.length()-1 && phrase[index] == phrase[index+1] ){
-    repetition++;
-    index++;
-  }
-  return repetition;
+Password::Password() {
+    // Default password initialization
+    pass_history.push_back("ChicoCA-95929");
+}
+
+int Password::count_leading_characters(string phrase) {
+    if (phrase.empty()) return 0; // Handling empty strings
+
+    int repetition = 1;
+    int index = 0;
+
+    while (index < phrase.length() - 1 && phrase[index] == phrase[index + 1]) {
+        repetition++;
+        index++;
+    }
+    return repetition;
+}
+
+bool Password::has_mixed_case(string phrase) {
+    bool has_upper = false;
+    bool has_lower = false;
+    for (char c : phrase) {
+        if (isupper(c)) has_upper = true;
+        if (islower(c)) has_lower = true;
+        if (has_upper && has_lower) return true;
+    }
+    return false;
+}
+
+void Password::set(string new_password) {
+    if (new_password.length() < 8 || new_password.length() > 20) return;
+    if (count_leading_characters(new_password) > 3) return;
+    if (!has_mixed_case(new_password)) return;
+    if (std::find(pass_history.begin(), pass_history.end(), new_password) != pass_history.end()) return;
+
+    // Set the new password
+    pass_history.push_back(new_password);
+}
+
+bool Password::authenticate(string test_password) {
+    if (pass_history.empty()) return false;
+    return pass_history.back() == test_password;
 }
--- PasswordTest.cpp
+++ PasswordTest.cpp
@@ -1,22 +1,77 @@
-/**
- * Unit Tests for Password class
-**/
-
 #include <gtest/gtest.h>
 #include "Password.h"
 
-class PracticeTest : public ::testing::Test
-{
-	protected:
-		PracticeTest(){} //constructor runs before each test
-		virtual ~PracticeTest(){} //destructor cleans up after tests
-		virtual void SetUp(){} //sets up before each test (after constructor)
-		virtual void TearDown(){} //clean up after each test, (before destructor)
+class PasswordTest : public ::testing::Test {
+protected:
+    PasswordTest(){} //constructor runs before each test
+    virtual ~PasswordTest(){} //destructor cleans up after tests
+    virtual void SetUp(){} //sets up before each test (after constructor)
+    virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-TEST(PasswordTest, single_letter_password)
-{
-	Password my_password;
-	int actual = my_password.count_leading_characters("Z");
-	ASSERT_EQ(1, actual);
+TEST(PasswordTest, Single_Letter_Password) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("Z");
+    ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, Multiple_Leading_Characters) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("AAAA");
+    ASSERT_EQ(4, actual);
+}
+
+TEST(PasswordTest, No_Repeating_Characters) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("ABCDEFG");
+    ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, Empty_String) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("");
+    ASSERT_EQ(0, actual);
+}
+
+TEST(PasswordTest, Mixed_Case_Sensitivity) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("AaAaA");
+    ASSERT_EQ(1, actual);
+}
+
+TEST(PasswordTest, Special_Characters) {
+    Password my_password;
+    int actual = my_password.count_leading_characters("!!!@@@");
+    ASSERT_EQ(3, actual);
+}
+
+TEST(PasswordTest, MixedCaseValidation_True) {
+    Password my_password;
+    bool actual = my_password.has_mixed_case("Aa");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PasswordTest, MixedCaseValidation_False) {
+    Password my_password;
+    bool actual = my_password.has_mixed_case("AAA");
+    ASSERT_FALSE(actual);
+}
+
+TEST(PasswordTest, Authentication_Success) {
+    Password my_password;
+    my_password.set("NewPassword1");
+    bool actual = my_password.authenticate("NewPassword1");
+    ASSERT_TRUE(actual);
+}
+
+TEST(PasswordTest, Authentication_Failure) {
+    Password my_password;
+    my_password.set("NewPassword1");
+    bool actual = my_password.authenticate("newpassword1");
+    ASSERT_FALSE(actual);
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
